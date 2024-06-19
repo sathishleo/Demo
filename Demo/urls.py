fe9e38f3d75392e83a8788156b7082dd2aac5eed
sathishleo <sathishlesa@gmail.com>
2024-06-19 21:12:31 +0530

    api root changes

--- Demo/urls.py
+++ Demo/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from .swagger import schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usermodule/', include('usermodule.urls')),
-    path('userservice/', include('userservice.urls')),
+    path('api/', include('usermodule.urls')),
+    path('auth/', include('userservice.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
