3c41b782651c1217d8ee1546b76aedcbac7c4583
sathishleo <sathishlesa@gmail.com>
2024-08-06 17:16:14 +0530

    06-08-2024 point in backend

--- Demo/settings.py
+++ Demo/settings.py
@@ -103,7 +103,7 @@ GRAPH_MODELS ={
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
+        'NAME': 'mydatabase1',
         'USER': 'admin',
         'PASSWORD': 'P4ssw0rd!',
         'HOST': 'database-2.cp02qkug8ip9.ap-south-1.rds.amazonaws.com',
