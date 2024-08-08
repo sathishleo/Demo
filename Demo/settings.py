52245d8be9ba1cf57f6aee5496875716601c302e
sathishleo <sathishlesa@gmail.com>
2024-08-08 19:11:36 +0530

    08-08-2024 point in backend

--- Demo/settings.py
+++ Demo/settings.py
@@ -180,7 +180,7 @@ ALLOWED_HOSTS = ['3.108.224.21']
 
 # CORS settings (if needed)
 CORS_ALLOWED_ORIGINS = [
-    "https://xraydoc.netlify.app/api/",
+    "https://xraydoc.netlify.app/api",
 ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
